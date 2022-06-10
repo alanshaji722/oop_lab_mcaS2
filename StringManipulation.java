cd74237c6fc820988fba94b6ec7b4e2f4a169e14
Alan <94525984+alanshaji722@users.noreply.github.com>
2022-06-10 09:34:24 +0530

    Add files via upload

--- StringManipulation.java
+++ StringManipulation.java
@@ -1,33 +1,36 @@
-
-package stringmanipulation;
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringmanupilation;
 import java.util.Scanner;
-
-
-public class StringManipulation 
-{
-  public static void main(String args[]) 
-  {
-    System.out.println("Input a String:");
-    Scanner sc = new Scanner(System.in);
-    String s = sc.nextLine();
-    System.out.println("Resulting String:");
-    System.out.println(display(s));
-  }
-  
- public static String display(String s) 
- {
-    StringBuffer sb = new StringBuffer();
-    char a = s.charAt(0);
-    char b = s.charAt(1);
-  if(a=='j' && b=='b')
-     sb.append(s.substring(0));
-  else if (a != 'j' && b == 'b')
-      sb.append(s.substring(1));
-  if (a != 'j' && b != 'b')
-      sb.append(s.substring(2));
-  else
-      sb.append("j").append(s.substring(2));
-   return sb.toString();
-  }
+public class StringManipulation {
+    String str1,str2;    
+    StringManipulation(String s1){
+        str1=s1;        
+    }    
+    public void strLen(){
+        System.out.println("\n The length of string1 :"+str1.length());              
+    }    
+    public void strConcat(){
+        System.out.println("\n Concatenated String 'HELLO':"+(str1.concat("HELLO")));        
+    }    
+    public void charpos(){
+        System.out.println("\n Character at position 4 in string1:"+(str1.charAt(3)));        
+    }    
+    public void compare(){
+        System.out.println("\n Compare to 'JAVA '"+(str1.compareTo("JAVA")));
+    }    
+    public static void main(String[] args) {
+        String str;
+        Scanner sc =new Scanner(System.in);        
+        System.out.println("Enter the String");
+        str=sc.nextLine();       
+        StringManipulation s1=new StringManipulation(str);
+                s1.strLen();
+                s1.strConcat();
+                s1.charpos();
+                s1.compare();                               
+    }    
 }
